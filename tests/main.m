d9cd49f0af20fb82df8f60552af672182180ba63
seo <seo@psoft.co.jp>
2013-02-27 11:44:41 +0900

    Initial Commit

---
+++ tests/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  tests
+//
+//  Created by 瀬尾 広道 on 2013/02/27.
+//  Copyright (c) 2013年 H.Seo. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+	@autoreleasepool {
+	    
+	    // insert code here...
+	    NSLog(@"Hello, World!");
+	    
+	}
+    return 0;
+}
+
